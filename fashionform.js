5d69776d482fd39f7fb46f5ad621ab5abc81d306
janeflair1 <44422896+janeflair1@users.noreply.github.com>
2019-02-14 12:51:26 -0500

    fashion website

---
+++ fashionform.js
@@ -0,0 +1,17 @@
+
+    if(typeof wabtn4fg==="undefined"){
+        wabtn4fg=1;
+        h=document.head||document.getElementsByTagName("head")[0],s=document.createElement("script");
+        s.type="text/javascript";
+        s.src="https://your-domain/wp-content/themes/your-theme/whatsapp.js";
+        h.appendChild(s);
+    }
+/*when you start typeing text shows on 
+inner HTML
+an id on top of input
+i need a span 
+create an element let first = $("") $(<li></li>)
+$().append()
+when click of a key in input i  need text to show
+when out of form the button disappears
+*/
